3d2f16291561526f3117cfcd26e79eee810d1806
Omid Nejadabbasi <nejadabbasio@gmail.com>
2022-04-05 13:11:08 +0430

    do some requests to the server

--- main.js
+++ main.js
@@ -1,1 +1,49 @@
-console.log('hello world')
+const ActiveDirectory = require('activedirectory')
+
+const username = 'OmidNe@systemgroup.net'
+const password = require('./password')
+const config = {
+    url: 'ldap://systemgroup.net',
+    baseDN: 'dc=systemgroup,dc=com',
+    username: username,
+    password: password,
+}
+
+let client = ActiveDirectory(config)
+
+client.authenticate(username, password, (auth, err) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+
+    console.log(auth)
+})
+
+var query = 'cn=*Exchange*';
+var opts = {
+    includeMembership: ['group', 'user'], // Optionally can use 'all'
+    includeDeleted: false
+};
+
+client.find(query, function(err, results) {
+    if ((err) || (!results)) {
+        console.log('ERROR: ' + JSON.stringify(err));
+        return;
+    }
+
+    console.log('Groups');
+    _.each(results.groups, function(group) {
+        console.log('  ' + group.cn);
+    });
+
+    console.log('Users');
+    _.each(results.users, function(user) {
+        console.log('  ' + user.cn);
+    });
+
+    console.log('Other');
+    _.each(results.other, function(other) {
+        console.log('  ' + other.cn);
+    });
+});
---
+++ password.js
@@ -0,0 +1,1 @@
+module.exports = 'Rin9uRCzhLuzm4X'
